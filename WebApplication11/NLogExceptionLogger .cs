ac20e0b5200f8076c7e6f776be21b927805edea7
Abdul Rehman <rehman.a@live.com>
2018-09-11 23:11:28 -0400

    Add project files.

---
+++ WebApplication11/NLogExceptionLogger .cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace WebApplication11
+{
+    public class NLogExceptionLogger : ExceptionLogger
+    {
+        private static readonly Logger Nlog = LogManager.GetCurrentClassLogger();
+        public override void Log(ExceptionLoggerContext context)
+        {
+            Nlog.Log(LogLevel.Error, context.Exception, RequestToString(context.Request));
+        }
+
+        private static string RequestToString(HttpRequestMessage request)
+        {
+            var message = new StringBuilder();
+            if (request.Method != null)
+                message.Append(request.Method);
+
+            if (request.RequestUri != null)
+                message.Append(" ").Append(request.RequestUri);
+
+            return message.ToString();
+        }
+    }
+}
---
+++ WebApplication11/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using Microsoft.Owin;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.WebApi;
+using Owin;
+using WebApplication11.App_Start;
+
+[assembly: OwinStartup(typeof(WebApplication11.Startup))]
+
+namespace WebApplication11
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var container = UnityConfig.GetConfiguredContainer();
+
+            config.DependencyResolver = new UnityHierarchicalDependencyResolver(container);
+            config.MessageHandlers.Add(new ContextBuilderHandler());
+            config.Services.Add(typeof(IExceptionLogger), new NLogExceptionLogger());
+
+            app.UseWebApi(config);
+        }        
+    }
+
+    public class MyContext
+    {
+        public Uri RequestUri { get; set; }
+    }
+
+    public class ContextBuilderHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var ctx = new MyContext
+            {
+                RequestUri = request.RequestUri
+            };
+
+            throw new Exception("oops");
+
+            var container = request.GetDependencyScope().GetService(typeof(IUnityContainer)) as IUnityContainer;
+
+            container.RegisterInstance(ctx);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+
+}
